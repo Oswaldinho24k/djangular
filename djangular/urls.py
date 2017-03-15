842ee790ff15b2876df473efb2ea49e63b7e6a75
Oswaldo <oswalfut_96@hotmail.com>
2017-03-15 13:56:00 -0600

    filters :boom:

--- djangular/urls.py
+++ djangular/urls.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name="scrumboard/home.html"))), 
+    url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name="home.html"))), 
     url(r'^scrumboard/',
     	include(scrumUrls)),
     url(r'^auth_api/', include(auth_apiUrls)), 
--- static/js/scrumboard.js
+++ static/js/scrumboard.js
@@ -1,10 +1,10 @@
 (function(){
 		'use strict';
 
-		angular.module('scrumboard.demo', [])
-			.controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
+		angular.module('scrumboard.demo', ['ngRoute'])
+			.controller('ScrumboardController', ['$scope', '$http','$location',  ScrumboardController]);
 
-			function ScrumboardController($scope, $http){
+			function ScrumboardController($scope, $http, $location){
 				$scope.add = function(list, title){
 					var card = {
 						list: list.id, 
@@ -21,11 +21,13 @@
 							alert('No se pudo papaê');
 						});
 				};
+				$scope.logout = function(){
+					$http.get('/auth_api/logout')
+						.then(function(){
+							$location.url('/login');
+						});
+				}
 
-				$scope.login = function(){
-					$http.post('/auth_api/login/', 
-						{username: 'oswaldinho', password: 'oswaldinho' })
-				};
 
 				$scope.data = [];
 				$http.get('/scrumboard/lists/').then(function(response){
