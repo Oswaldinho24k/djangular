1ff5aab393e89ce47ff1b7bf7a2af091ecd2dc8e
Oswaldo <oswalfut_96@hotmail.com>
2017-03-15 14:57:14 -0600

    move cards

--- static/js/card.directive.js
+++ static/js/card.directive.js
@@ -10,27 +10,46 @@
 			restrict: 'E',
 			controller: ['$scope', '$http', function($scope, $http){
 				var url = '/scrumboard/cards/' + $scope.card.id + '/';
+				$scope.destList = $scope.list;
 				$scope.update = function(){
-					$http.put(
+					return $http.put(
 						url, 
 						$scope.card
 					);
 					console.log('actualizando');
 				};//update
+				function removeCardFromList(card, list){
+					var cards = list.cards;
+					cards.splice(
+						cards.indexOf(card), 
+						1
+					);
+				}
+
 				$scope.delete = function(){
 					if(confirm('Seguro??? Ya no hay vuelta atrás heeeee'))
 					$http.delete(url).then(function(){
-						var cards = $scope.list.cards;
-						cards.splice(
-							cards.indexOf($scope.card),
-							1
-						);
-					})
+							removeCardFromList($scope.card, $scope.list);
+						
+					});
 				};//delete
 
 				$scope.modelOptions = {
 					debounce: 500
 				};
+
+				$scope.move = function(){
+					if ($scope.destList === undefined){
+						return;
+					}
+					$scope.card.list = $scope.destList.id;
+					$scope.update().then(function(){
+						{
+							removeCardFromList($scope.card, $scope.list);
+							$scope.destList.cards.push($scope.card);
+						}
+					});
+				}
 			}]
 		};
 	}
